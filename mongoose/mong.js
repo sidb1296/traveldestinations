c08fb10b4b76e582e7a087806ec88b8f46aa1df5
sidb1296 <sidb1296@gmail>
2020-03-22 20:01:28 +0530

    mongoose deleted

--- mongoose/mong.js
+++
@@ -1,9 +0,0 @@
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
-
-var kittySchema = new mongoose.Schema({
-    name: String
-  });
-
-  var Kitten = mongoose.model('Kitten', kittySchema);
